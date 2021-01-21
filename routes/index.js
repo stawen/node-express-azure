7915beddf0d21495f74c174e6fc7b4d55c6638a4
Stanislas DRAUNET <stanislas.draunet@maif.fr>
2021-01-21 21:48:11 +0100

    add AppID Env Var

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,10 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', {
-    title: 'PaaS MAIF - GO 2 CLOUD',   
-  });
+    res.render('index', {
+        title: 'PaaS MAIF - GO 2 CLOUD',
+        appId: process.env.APPID || '0'
+    });
 });
 
 module.exports = router;
