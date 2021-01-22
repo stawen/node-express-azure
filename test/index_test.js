bfd2a7a076f41dd465455d085c15a84e70ce2fb3
Stanislas DRAUNET <stanislas.draunet@maif.fr>
2021-01-22 08:56:29 +0100

    change test unit

--- test/index_test.js
+++ test/index_test.js
@@ -13,7 +13,7 @@ describe('/GET', () => {
             .get('/')
             .end((err, res) => {
                 res.should.have.status(200);
-                res.text.should.contain('GO 2 CLOUD');
+                res.text.should.contain('PaaS MAIF');
                 done();
             });
     });
